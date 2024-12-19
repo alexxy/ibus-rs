8a65b7a135e01172d4837a019739dd33b0920bd3
Alexey Shvetsov <alexxyum@gmail.com>
2024-12-19 09:21:21 +0300

    Update license headers
    Signed-off-by: Alexey Shvetsov <alexxyum@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright 2024 Alexey 'alexxy' Shvetsov
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 pub struct IbusParser {
     buffer: [u8; 32],
     index: usize,
